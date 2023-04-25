151d116358aa42c52e3d1281678a0ff909ae4b31
Wyatt Eberspacher <wseberspacher@gmail.com>
2023-04-24 22:26:00 -0700

    Initial Commit

---
+++ ImageDownload/ImageDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageDownloadApp.swift
+//  ImageDownload
+//
+//  Created by Wyatt on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
